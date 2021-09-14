b8882ffa450ef65e3c01efccc08deb6663f3be34
https://github.com/AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-09-14 10:28:20 -0700

    problem 8 solved

--- js/app.js
+++ js/app.js
@@ -19,8 +19,11 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p>rating: ${product.rating.rate}</p>
-      <h2>Price: $ ${product.price}</h2>
+      <div class="rating">
+      <p>Rate: ${product.rating.rate}</p>
+      <p>Count: ${product.rating.count}</p>
+    </div>
+      <h2 class="price">Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
